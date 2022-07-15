f0b8bcdb50ee36de8acbee7e4b8f33f64abb9809
jujuwon <webmon@kakao.com>
2022-07-15 14:47:49 +0900

    feat: Add Member service test

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -12,26 +12,26 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
-
-    @Autowired MemberRepository memberRepository;
-
-    @Test
-    @Transactional
-    @Rollback(value = false)
-    public void testMember() throws Exception {
-        //given
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member); // JPA 동일성 확인
-        System.out.println("findMember == member : " + (findMember == member));
-        // 영속성 컨택스트 -> 같은 영속성 컨텍스트 안에서는 id값(식별자)이 같으면 같은 엔티티로 식별
-    }
+//
+//    @Autowired MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional
+//    @Rollback(value = false)
+//    public void testMember() throws Exception {
+//        //given
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        //when
+//        Long savedId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(savedId);
+//
+//        //then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        Assertions.assertThat(findMember).isEqualTo(member); // JPA 동일성 확인
+//        System.out.println("findMember == member : " + (findMember == member));
+//        // 영속성 컨택스트 -> 같은 영속성 컨텍스트 안에서는 id값(식별자)이 같으면 같은 엔티티로 식별
+//    }
 }
